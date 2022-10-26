62ced420d738818c6264e3d086db567f54780183
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-10-26 12:40:30 +0300

    added functionality

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,8 @@
 import './Header.css';
-const Header = () => {
+const Header = (props) => {
     return <div className='container'> 
         <h1 className="header"> Counter app </h1>
+        <h2>{props.name}</h2>
     </div>
   
   }
--- src/Main.jsx
+++ src/Main.jsx
@@ -2,17 +2,46 @@ import "./Main.css";
 import React, { Component } from "react";
 
 class Main extends Component {
-    state = {
-        counter: 0 ,
+  state = {
+    counter: 0,
+  };
+
+  addOneHandler = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  addFiveHandler = () => {
+    this.setState({ counter: this.state.counter + 5 });
+  };
+  removeOneHandler = () => {
+    if (this.state.counter > 0) {
+      this.setState({ counter: this.state.counter - 1 });
+    } else {
+      this.setState({ counter: 0 });
     }
-    addOneHandler = ()=>{
-        this.setState({counter:this.state.counter+1})
+  };
+  removeFiveHandler = () => {
+    if (this.state.counter > 5) {
+      this.setState({ counter: this.state.counter - 5 });
+    } else {
+      this.setState({ counter: 0 });
     }
+  };
+  resetHandler = () => {
+    this.setState({ counter: 0 });
+  };
   render() {
+    let circleClass=""
+    if (this.state.counter === 0){
+      circleClass="circle"
+    } else if(this.state.counter % 2 === 0) {
+      circleClass="circle even"
+    } else {
+      circleClass="circle odd"
+    }
     return (
       <main>
         <div className="container">
-          <div className="circle">
+          <div className={circleClass}>
             <div>
               <h1 className="counter">{this.state.counter}</h1>
             </div>
@@ -20,11 +49,11 @@ class Main extends Component {
         </div>
 
         <div className="btns">
-          <button>Add Five</button>
+          <button onClick={this.addFiveHandler}>Add Five</button>
           <button onClick={this.addOneHandler}>Add One</button>
-          <button>Reset</button>
-          <button>Remove One</button>
-          <button>Remove Five</button>
+          <button onClick={this.resetHandler}>Reset</button>
+          <button onClick={this.removeOneHandler}>Remove One</button>
+          <button onClick={this.removeFiveHandler}>Remove Five</button>
         </div>
       </main>
     );
